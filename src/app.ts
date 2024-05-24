6c2bbdd72d8878bf1c7b95f18f6d7fd8ef28a85b
fai-sas <craftedbyfaisal@gmail.com>
2024-05-24 10:39:30 +0600

    middlewares added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
 import { StudentRoutes } from './app/modules/student/student.route'
+import notFound from './app/middlewares/notFound'
+import globalErrorHandler from './app/middlewares/globalErrorhandler'
 const app: Application = express()
 
 //parsers
@@ -11,7 +13,10 @@ app.use(cors())
 app.use('/api/v1/students', StudentRoutes)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('University Management Application')
 })
+
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app
