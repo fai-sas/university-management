f9e1dfc49cdf7e01779d2fa9bcc7a528edd9c7bd
fai-sas <craftedbyfaisal@gmail.com>
2024-05-25 22:58:24 +0600

    catchAsync function added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,20 @@
-import { NextFunction, Request, Response } from 'express'
 import status from 'http-status'
 import sendResponse from '../../utils/sendResponse'
 import { UserServices } from './user.service'
+import catchAsync from '../../utils/catchAsync'
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { password, student: studentData } = req.body
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body
 
-    const result = await UserServices.createStudentIntoDB(password, studentData)
+  const result = await UserServices.createStudentIntoDB(password, studentData)
 
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  })
+})
 
 export const UserControllers = {
   createStudent,
