7bfbbb5a7d5e1ba7fd9ec3f317ddf20323fc06a5
fai-sas <craftedbyfaisal@gmail.com>
2024-05-29 21:48:04 +0600

    updated academic department model

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -34,6 +34,17 @@ academicDepartmentSchema.pre('save', async function (next) {
   next()
 })
 
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery()
+  const isDepartmentExist = await AcademicDepartment.findOne(query)
+
+  if (!isDepartmentExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This department does not exist! ')
+  }
+
+  next()
+})
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema
