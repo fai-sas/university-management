b9b2879fd1e2c32eaa3e220bace0704d7e8319dd
fai-sas <craftedbyfaisal@gmail.com>
2024-05-30 19:32:20 +0600

    implemented transaction and rollback in create user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import { AcademicSemester } from '../academicSemester/academicSemester.model'
 import { TStudent } from '../student/student.interface'
@@ -5,6 +6,8 @@ import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 import { generateStudentId } from './user.utils'
+import AppError from '../../errors/AppError'
+import httpStatus from 'http-status'
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create an user object
@@ -21,22 +24,39 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester
   )
 
-  // set manually generated id
-  // userData.id = '2030100001'
+  const session = await mongoose.startSession()
 
-  //set  generated id
-  userData.id = await generateStudentId(admissionSemester)
+  try {
+    session.startTransaction()
+    //set generated id
+    userData.id = await generateStudentId(admissionSemester)
 
-  // create an user
-  const newUser = await User.create(userData)
+    // create an user (transaction-1)
+    const newUser = await User.create([userData], { session }) // array
 
-  if (Object.keys(newUser).length) {
-    // set id, _id as user
-    payload.id = newUser.id
-    payload.user = newUser._id // reference id
+    // create a student
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+    }
 
-    const newStudent = await Student.create(payload)
-    return newStudent
+    // set id, id as user
+
+    payload.id = newUser[0].id // auto generated id
+    payload.user = newUser[0]._id // reference id
+
+    // create a student (transaction-2)
+    const newStudent = await Student.create([payload], { session }) // array
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+  } catch (error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student')
   }
 }
 
