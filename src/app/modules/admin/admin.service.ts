6031a37fbf94a9aac68bb765d7f29fbab8c4b044
fai-sas <craftedbyfaisal@gmail.com>
2024-06-01 22:33:07 +0600

    get single admin service added

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,5 +1,12 @@
+import { Admin } from './admin.model'
+
 const getAllAdminsFromDB = async (query: Record<string, unknown>) => {}
-const getSingleAdminFromDB = async (adminId: string) => {}
+
+const getSingleAdminFromDB = async (_id: string) => {
+  const result = await Admin.findById({ _id })
+  return result
+}
+
 const updateAdminIntoDB = async () => {}
 const deleteAdminFromDB = async () => {}
 
