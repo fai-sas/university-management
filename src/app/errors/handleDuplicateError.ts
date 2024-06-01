5cb9f2747357e422e976a3b300e13f8470739ce5
fai-sas <craftedbyfaisal@gmail.com>
2024-06-01 19:40:01 +0600

    custom duplicate error handler added

---
+++ src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,29 @@
+import {
+  TErrorSources,
+  TGenericErrorResponse,
+} from '../interface/error.interface'
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  // Extract value within double quotes using regex
+  const match = err.message.match(/"([^"]*)"/)
+
+  // The extracted value will be in the first capturing group
+  const extractedMessage = match && match[1]
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: `${extractedMessage} already exists`,
+    },
+  ]
+
+  const statusCode = 400
+
+  return {
+    statusCode,
+    message: 'This value already exists',
+    errorSources,
+  }
+}
+
+export default handleDuplicateError
