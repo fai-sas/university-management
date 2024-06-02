283f83f04ea7feb39762ad6947e892be1a4087fc
fai-sas <craftedbyfaisal@gmail.com>
2024-06-02 22:02:37 +0600

    get all faculties service added

--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -1,8 +1,25 @@
+import QueryBuilder from '../../builder/QueryBuilder'
+import { FacultySearchableFields } from './faculty.constant'
 import { TFaculty } from './faculty.interface'
+import { Faculty } from './faculty.model'
 
-const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {}
+const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {
+  const facultyQuery = new QueryBuilder(Faculty.find(), query)
+    .search(FacultySearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields()
+
+  const result = await facultyQuery.modelQuery
+
+  return result
+}
+
 const getSingleFacultyFromDB = async (id: string) => {}
+
 const updateFacultyIntoDB = async (id: string, payload: Partial<TFaculty>) => {}
+
 const deleteFacultyFromDB = async (id: string) => {}
 
 export const FacultyServices = {
