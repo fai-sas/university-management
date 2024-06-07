d615e0c52616fa5a1c7d1832c52dc33a3076e9c4
fai-sas <craftedbyfaisal@gmail.com>
2024-06-07 18:39:19 +0600

    offered course controllers added

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -1,13 +1,22 @@
-const createOfferedCourseIntoDB = async () => {}
-const getAllOfferedCoursesFromDB = async () => {}
-const getSingleOfferedCourseFromDB = async () => {}
-const deleteOfferedCourseFromDB = async () => {}
-const updateOfferedCourseIntoDB = async () => {}
+import { TOfferedCourse } from './offeredCourse.interface'
+
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {}
+
+const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {}
+
+const getSingleOfferedCourseFromDB = async (id: string) => {}
+
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Pick<TOfferedCourse, 'faculty' | 'days' | 'startTime' | 'endTime'>
+) => {}
+
+const deleteOfferedCourseFromDB = async (id: string) => {}
 
 export const OfferedCourseServices = {
   createOfferedCourseIntoDB,
   getAllOfferedCoursesFromDB,
   getSingleOfferedCourseFromDB,
-  deleteOfferedCourseFromDB,
   updateOfferedCourseIntoDB,
+  deleteOfferedCourseFromDB,
 }
