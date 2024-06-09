92638ce6575fb223dcd7a8bd1fa50c7e5ba367b1
fai-sas <craftedbyfaisal@gmail.com>
2024-06-09 21:44:33 +0600

    user model updated with statics

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt'
 import { Schema, model } from 'mongoose'
 import config from '../../config'
-import { TUser } from './user.interface'
+import { TUser, UserModel } from './user.interface'
 
-const userSchema = new Schema<TUser>(
+const userSchema = new Schema<TUser, UserModel>(
   {
     id: {
       type: String,
@@ -12,10 +12,14 @@ const userSchema = new Schema<TUser>(
     password: {
       type: String,
       required: true,
+      select: 0,
     },
     needsPasswordChange: {
       type: Boolean,
       default: true,
+    },
+    passwordChangedAt: {
+      type: Date,
     },
     role: {
       type: String,
@@ -52,4 +56,25 @@ userSchema.post('save', function (doc, next) {
   next()
 })
 
-export const User = model<TUser>('User', userSchema)
+userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+  return await User.findOne({ id }).select('+password')
+}
+
+userSchema.statics.isPasswordMatched = async function (
+  plainPassword: string,
+  hashedPassword: string
+) {
+  return await bcrypt.compare(plainPassword, hashedPassword)
+}
+
+userSchema.statics.isJWTIssuedBeforePasswordChanged = async function (
+  passwordChangedTimestamp: Date,
+  jwtIssuedTimestamp: number
+) {
+  const passwordChangedTime =
+    new Date(passwordChangedTimestamp).getTime() / 1000
+
+  return passwordChangedTime > jwtIssuedTimestamp
+}
+
+export const User = model<TUser, UserModel>('User', userSchema)
