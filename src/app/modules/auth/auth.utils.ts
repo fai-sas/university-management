b01347bac0e510664aff3802d3bbe2392e55bdbe
fai-sas <craftedbyfaisal@gmail.com>
2024-06-09 19:30:02 +0600

    jwt utility function added

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -1,11 +1,14 @@
-const loginUser = async () => {}
+import jwt from 'jsonwebtoken'
 
-const changePassword = async () => {}
-
-const refreshToken = async () => {}
-
-export const AuthServices = {
-  loginUser,
-  changePassword,
-  refreshToken,
+export const createToken = (
+  jwtPayload: {
+    userId: string
+    role: string
+  },
+  secret: string,
+  expiresIn: string
+) => {
+  return jwt.sign(jwtPayload, secret, {
+    expiresIn,
+  })
 }
