d9219a8e26695556bb3d0f0b32d6c1f56be7537a
fai-sas <craftedbyfaisal@gmail.com>
2024-06-09 22:13:30 +0600

    change password service added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import { JwtPayload } from 'jsonwebtoken'
 import { TLoginUser } from './auth.interface'
 import { User } from '../user/user.model'
@@ -62,7 +63,50 @@ const loginUser = async (payload: TLoginUser) => {
 const changePassword = async (
   userData: JwtPayload,
   payload: { oldPassword: string; newPassword: string }
-) => {}
+) => {
+  // check if the user is exist
+  const user = await User.isUserExistsByCustomId(userData.userId)
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !')
+  }
+
+  // check if the user is already deleted
+  const isDeleted = user?.isDeleted
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !')
+  }
+
+  // check if the user is blocked
+  const userStatus = user?.status
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !')
+  }
+
+  //check if the password is correct
+  if (!(await User.isPasswordMatched(payload.oldPassword, user?.password)))
+    throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched')
+
+  //hash new password
+  const newHashedPassword = await bcrypt.hash(
+    payload.newPassword,
+    Number(config.bcrypt_salt_rounds)
+  )
+
+  await User.findOneAndUpdate(
+    {
+      id: userData.id,
+      role: userData.role,
+    },
+    {
+      password: newHashedPassword,
+      passwordChangedAt: new Date(),
+    }
+  )
+  return null
+}
 
 const refreshToken = async (token: string) => {}
 
